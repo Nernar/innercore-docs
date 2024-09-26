deff3dd7a0c4a37b22d7f67c928a41e61a14b22d
MaXFeeD <maksfeed.mf@gmail.com>
2024-09-26 03:03:53 +0300

    2.3.1b116-3: NetworkEntity.set(Dis)ConnectionPlayerListener, getType

--- core-engine/NetworkEntity.d.ts
+++ core-engine/NetworkEntity.d.ts
@@ -1,3 +1,15 @@
+declare namespace NetworkEntity {
+	/**
+	 * Listener used to handle type events.
+	 */
+	interface IPlayerListener {
+		/**
+		 * @param client (dis)connected client
+		 */
+		(client: NetworkClient): void
+	}
+}
+
 /**
  * Class that represents network entity of the block, currently is not learned.
  */
@@ -6,4 +18,13 @@ declare class NetworkEntity {
 	remove(): void;
 	send(name: string, data: any): void;
 	getClients(): NetworkConnectedClientList;
+	/**
+	 * @since 2.3.1b116-3
+	 */
+	setConnectionPlayerListener(listener: NetworkEntity.IPlayerListener): void;
+	/**
+	 * @since 2.3.1b116-3
+	 */
+	setDisconnectionPlayerListener(listener: NetworkEntity.IPlayerListener): void;
+	getType(): NetworkEntityType;
 }
