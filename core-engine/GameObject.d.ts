372707ce07967894223d5e2638469c7235af6a37
MaXFeeD <maksfeed.mf@gmail.com>
2024-10-08 10:54:36 +0300

    GameObject internals, docs improvements

--- core-engine/GameObject.d.ts
+++ core-engine/GameObject.d.ts
@@ -12,26 +12,45 @@ declare class GameObject<T extends GameObjectPrototype> {
     constructor(type: string, prototype: T);
 
     /**
-     * Original value passed to {@link GameObject.constructor}.
+     * Original value passed to {@link GameObject.constructor} as type.
      */
     readonly originalName: string;
-
-    readonly gameobjectName: string;
-
-    readonly isInstance: boolean;
-
     /**
-     * `true` if current GameObject was deployed, `false` otherwise.
+     * Same as {@link GameObject.originalName}, but with suffix
+     * which are used for object saving to be unique.
+     */
+    readonly gameobjectName: string;
+    /**
+     * Used when {@link Saver.registerObject} calling inside.
+     * @internal
+     */
+    readonly saverId: number;
+    /**
+     * `true` if current object is instance, prototype otherwise.
+     */
+    readonly isInstance: boolean;
+    /**
+     * `true` if current object was deployed or added directly.
      */
     readonly isDeployed: boolean;
-
+    /**
+     * Prototype from which instantiated objects are created.
+     * @internal
+     */
     readonly Prototype: T;
 
     /**
-     * Creates a new game object with specified params and registers it for saving
-     * and as an {@link Updatable}.
-     * @param args any arguments that are passed to {@link GameObjectPrototype.init}
-     * function
+     * Creates a new game object without deploying it; you are shouldn't
+     * do it normally, it was called by {@link GameObject.deploy}.
+     * @returns Instantiated game object.
+     * @internal
+     */
+    instantiate(): GameObject<T>;
+
+    /**
+     * Creates a new game object with specified params and registers
+     * it for saving and as an {@link Updatable}.
+     * @param args any arguments that are passed to {@link GameObjectPrototype.init} function
      * @returns Instantiated game object.
      */
     deploy(...args: any): GameObject<T>;
@@ -62,27 +81,82 @@ interface GameObjectPrototype extends Updatable {
 
 declare namespace GameObjectRegistry {
     /**
-     * Gets an array of {@link GameObject GameObjects} of specified type.
+     * Game objects which are registered via {@link GameObject.constructor}
+     * or internally via {@link GameObjectRegistry.registerClass}.
+     * @internal
+     */
+    const gameObjectTypes: {
+        [gameobjectName: string]: GameObject<any>
+    };
+    /**
+     * Game objects which are deployed via {@link GameObject.deploy}
+     * or internally via {@link GameObjectRegistry.deployGameObject}
+     * and {@link GameObjectRegistry.addGameObject}.
+     * @internal
+     */
+    const activeGameObjects: {
+        [type: string]: GameObject<any>
+    };
+
+    /**
+     * Suffixes game object type if it already registered.
+     * @internal
+     */
+    function genUniqueName(name: string): string;
+
+    /**
+     * Registers game object to be saveable, called from {@link GameObject.constructor}.
+     * @internal
+     */
+    function registerClass(gameObject: GameObject<any>): void;
+
+    /**
+     * Puts game object to an updatable queue, calling {@link GameObjectPrototype.loaded}.
+     * Should be called by {@link GameObject.deploy}.
+     * @internal
+     */
+    function deployGameObject(gameObject: GameObject<any>): void;
+
+    /**
+     * Puts game object as active, but without adding as updatable, which
+     * has been achieved by calling {@link GameObject.deploy}.
+     * @internal
+     */
+    function addGameObject(gameObject: GameObject<any>): void;
+
+    /**
+     * Destroys game object, but without removing updatable as
+     * it is done via {@link GameObject.destroy}.
+     * @internal
+     */
+    function removeGameObject(gameObject: GameObject<any>): void;
+
+    /**
+     * Clears active game object list, maintaining updatable states.
+     * @internal
+     */
+    function resetEngine(): void;
+
+    /**
+     * Gets an array of deployed {@link GameObject game objects} of specified type.
      * @param type unique {@link GameObject} type to get all the instances of
-     * @param clone if true, a new array is created to ensure the original
-     * engine's data safety.
+     * @param clone if `true`, a new array is created to be changeable,
+     * recommended by default
      */
     function getAllByType<T extends GameObjectPrototype>(type: string, clone: boolean): GameObject<T>[];
 
     /**
-     * Calls function of the {@link GameObject} of specified type with specified
-     * parameters.
+     * Calls function of the {@link GameObject} of specified type
+     * with specified parameters.
      * @param type unique {@link GameObject} type to get all the instances of
-     * @param func function name as defined in {@link GameObjectPrototype} passed to
-     * {@link GameObject.constructor}
+     * @param what function name as defined in {@link GameObjectPrototype}
+     * passed to {@link GameObject.constructor}
      * @param params parameters to be passed to the function
      */
-    function callForType(type: string, func: string, ...params: any): any;
+    function callForType(type: string, what: string, ...params: any): any;
 
     /**
-     * Same as {@link GameObjectRegistry.callForType}, though a new array is created
-     * before calling functions on the game objects to ensure the original engine's
-     * data safety.
+     * Same as {@link GameObjectRegistry.callForType} but for async operations.
      */
-    function callForTypeSafe(type: string, func: string, ...params: any): any;
+    function callForTypeSafe(type: string, what: string, ...params: any): any;
 }
